7994cd44b51fa112b7d01926205b29dc2afc21a8
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-11-22 18:46:06 -0500

    NameServer version 1

--- nameServer.cc
+++ nameServer.cc
@@ -1,1 +1,60 @@
 #include "nameServer.h"
+#include "printer.h"
+#include "vendingMachine.h"
+
+NameServer::NameServer(Printer &prt, unsigned int numVendingMachines, unsigned int numStudents) :
+    printer(prt),
+    numVendingMachines(numVendingMachines),
+    numStudents(numStudents),
+    numRegistered(0) {
+    machineList = new VendingMachine*[numVendingMachines];
+    machineAssignment = new unsigned int[numStudents];
+}
+NameServer::~NameServer() {
+    delete[] machineList;
+    delete[] machineAssignment;
+}
+
+void NameServer::VMregister(VendingMachine *vendingmachine) {
+    machineList[numRegistered] = vendingmachine;
+    printer.print(Printer::NameServer, (char)Register, vendingmachine->getId());
+}
+
+VendingMachine* NameServer::getMachine(unsigned int id) {
+    VendingMachine *ret = machineList[machineAssignment[id]];
+    printer.print(Printer::NameServer, (char)New, id, ret->getId());
+    machineAssignment[id] = (machineAssignment[id] < numVendingMachines) ? machineAssignment[id] + 1 : 0;
+    return ret;
+}
+
+VendingMachine** NameServer::getMachineList() {
+    return machineList;
+}
+
+void NameServer::main() {
+    printer.print(Printer::NameServer, (char)Start);
+    outer: while(true) {
+        while(numRegistered < numVendingMachines) {
+            _Accept(~NameServer) {
+                break outer;
+            } or _Accept(VMregister) {
+                numRegistered++;
+            }
+        }
+
+        for (unsigned int i = 0; i < numStudents; i++) {
+            unsigned int machine = i % numVendingMachines;
+            machineAssignment[i] = machine;
+        }
+
+        while(true) {
+            _Accept(~NameServer) {
+                break outer;
+            } or _Accept(getMachine) {
+            } or _Accept(getMachineList) {
+            }
+        }
+    }
+
+    printer.print(Printer::NameServer, (char)Finished);
+}
--- nameServer.h
+++ nameServer.h
@@ -5,9 +5,24 @@ _Monitor Printer;
 _Task VendingMachine;
 
 _Task NameServer {
+    enum States {
+        Starting = 'S',
+        Register = 'R',
+        New = 'N',
+        Finished = 'F'
+    };
+
+    Printer &printer;
+    unsigned int numVendingMachines;
+    unsigned int numStudents;
+    unsigned int numRegistered;
+    VendingMachine **machineList;
+    unsigned int *machineAssignment;
+
     void main();
   public:
     NameServer(Printer &prt, unsigned int numVendingMachines, unsigned int numStudents);
+    ~NameServer();
     void VMregister(VendingMachine *vendingmachine);
     VendingMachine *getMachine(unsigned int id);
     VendingMachine **getMachineList();
