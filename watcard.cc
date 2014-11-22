baa81359c4556d422977fc45a961157b308eef51
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-11-22 14:51:00 -0500

    Finish watcard

--- watcard.cc
+++ watcard.cc
@@ -1,19 +1,6 @@
 #include "watcard.h"
 
-
-WATCard::WATCard() : balance(0) {
-
-}
-
-WATCard::WATCard(const WATCard &toCopy) {
-    balance = toCopy.balance;
-}
-
-WATCard& WATCard::operator=(const WATCard &toAssign) {
-    WATCard newCard = new WATCard(toAssign);
-
-    return newCard;
-}
+WATCard::WATCard() : balance(0) { }
 
 void deposit(unsigned int amount) {
     balance += amount;
