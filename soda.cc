74d21b9e96fc98149bd1173a23362a1d8ce31892
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 14:58:49 -0500

    Add boilerplate for WATCardOffice.

--- soda.cc
+++ soda.cc
@@ -1,20 +1,3 @@
-_Task WATCardOffice {
-    struct Job {                           // marshalled arguments and return future
-        Args args;                         // call arguments (YOU DEFINE "Args")
-        WATCard::FWATCard result;          // return future
-        Job( Args args ) : args( args ) {}
-    };
-    _Task Courier { ... };                 // communicates with bank
-
-    void main();
-  public:
-    _Event Lost {};                        // lost WATCard
-    WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
-    WATCard::FWATCard create( unsigned int sid, unsigned int amount );
-    WATCard::FWATCard transfer( unsigned int sid, unsigned int amount, WATCard *card );
-    Job *requestWork();
-};
-
 _Monitor / _Cormonitor Printer {
   public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
