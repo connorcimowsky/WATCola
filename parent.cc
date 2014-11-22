cf683b0ade5e4cec0603e7211fa9184d05feced6
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 16:47:14 -0500

    Implement Parent.

--- parent.cc
+++ parent.cc
@@ -1,1 +1,34 @@
 #include "parent.h"
+#include "bank.h"
+#include "printer.h"
+#include "MPRNG.h"
+
+static const unsigned int AMOUNT_LOWER_BOUND = 1;
+static const unsigned int AMOUNT_UPPER_BOUND = 3;
+
+Parent::Parent(Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay) :
+    printer(prt),
+    bank(bank),
+    numStudents(numStudents),
+    parentalDelay(parentalDelay) {
+}
+
+void Parent::main() {
+    printer.print(Printer::Parent, Start);
+
+    while (true) {
+        _Accept(~Parent) {
+            break;
+        } _Else {
+            yield(parentalDelay);
+
+            unsigned int id = generator(numStudents);
+            unsigned int amount = generator(AMOUNT_LOWER_BOUND, AMOUNT_UPPER_BOUND);
+
+            printer.print(Printer::Parent, Deposit, id, amount);
+            bank.deposit(id, amount);
+        }
+    }
+
+    printer.print(Printer::Parent, Finished);
+}
--- parent.h
+++ parent.h
@@ -2,11 +2,18 @@
 #define PARENT_H
 
 _Monitor Printer;
+_Monitor Bank;
 
 _Task Parent {
-    void main();
-  public:
+public:
     Parent(Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay);
+private:
+    enum State { Start = 'S', Deposit = 'D', Finished = 'F' };
+    Printer &printer;
+    Bank &bank;
+    unsigned int numStudents;
+    unsigned int parentalDelay;
+    void main();
 };
 
 #endif
