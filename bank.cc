9f8868211a82624e35414515213d6bf39fe84a3c
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 15:40:03 -0500

    Implement Bank.

--- bank.cc
+++ bank.cc
@@ -1,1 +1,19 @@
 #include "bank.h"
+
+Bank::Bank(unsigned int numStudents) :
+    balances(new unsigned int[numStudents]()),
+    withdrawals(new uCondition[numStudents]) {
+}
+
+void Bank::deposit(unsigned int id, unsigned int amount) {
+    balances[id] += amount;
+    withdrawals[id].signal();
+}
+
+void Bank::withdraw(unsigned int id, unsigned int amount) {
+    while (balances[id] < amount) {
+        withdrawals[id].wait();
+    }
+
+    balances[id] -= amount;
+}
--- bank.h
+++ bank.h
@@ -6,6 +6,9 @@ public:
     Bank(unsigned int numStudents);
     void deposit(unsigned int id, unsigned int amount);
     void withdraw(unsigned int id, unsigned int amount);
+private:
+    unsigned int *balances;    // array of balances for each student
+    uCondition *withdrawals;   // array of conditions used for synchronizing withdrawals
 };
 
 #endif
