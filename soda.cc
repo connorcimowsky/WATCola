87e22118e127c6cae85a13dcc9f735c1ca30152f
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 14:42:15 -0500

    Add boilerplate for VendingMachine.

--- soda.cc
+++ soda.cc
@@ -26,21 +26,6 @@ _Task WATCardOffice {
     Job *requestWork();
 };
 
-_Task VendingMachine {
-    void main();
-  public:
-    enum Flavours { ... };                 // flavours of soda (YOU DEFINE)
-    _Event Funds {};                       // insufficient funds
-    _Event Stock {};                       // out of stock for particular flavour
-    VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
-                    unsigned int maxStockPerFlavour );
-    void buy( Flavours flavour, WATCard &card );
-    unsigned int *inventory();
-    void restocked();
-    _Nomutex unsigned int cost();
-    _Nomutex unsigned int getId();
-};
-
 _Task NameServer {
     void main();
   public:
