a9aa843ab92760174b9a943d121d8999abe7f952
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 14:40:21 -0500

    Add boilerplate for Bank.

--- soda.cc
+++ soda.cc
@@ -26,13 +26,6 @@ _Task WATCardOffice {
     Job *requestWork();
 };
 
-_Monitor Bank {
-  public:
-    Bank( unsigned int numStudents );
-    void deposit( unsigned int id, unsigned int amount );
-    void withdraw( unsigned int id, unsigned int amount );
-};
-
 _Task Parent {
     void main();
   public:
