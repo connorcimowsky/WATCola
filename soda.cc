02e76658dc42620cc41c3fc79dbb90d4d346215d
Connor Cimowsky <connorcimowsky@me.com>
2014-11-22 14:57:31 -0500

    Remove WATCard from soda.cc.

--- soda.cc
+++ soda.cc
@@ -1,14 +1,3 @@
-class WATCard {
-    WATCard( const WATCard & );            // prevent copying
-    WATCard &operator=( const WATCard & );
-  public:
-    WATCard();
-    typedef Future_ISM<WATCard *> FWATCard; // future watcard pointer
-    void deposit( unsigned int amount );
-    void withdraw( unsigned int amount );
-    unsigned int getBalance();
-};
-
 _Task WATCardOffice {
     struct Job {                           // marshalled arguments and return future
         Args args;                         // call arguments (YOU DEFINE "Args")
