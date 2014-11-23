145df46690e06bbd9a0fc0e92b04b46f759d0ee0
Connor Cimowsky <connorcimowsky@me.com>
2014-11-23 14:31:27 -0500

    Implement Student.

--- student.cc
+++ student.cc
@@ -1,1 +1,74 @@
 #include "student.h"
+#include "printer.h"
+#include "nameServer.h"
+#include "watcard.h"
+#include "watcardoffice.h"
+#include "vendingMachine.h"
+#include "MPRNG.h"
+
+static const unsigned int FLAVOUR_UPPER_BOUND = 3;
+static const unsigned int WATCARD_BALANCE = 5;
+
+Student::Student(Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
+    unsigned int maxPurchases) :
+    printer(prt), nameServer(nameServer), cardOffice(cardOffice), id(id), maxPurchases(maxPurchases) {
+}
+
+void Student::main() {
+    unsigned int bottles = generator(1, maxPurchases);
+    unsigned int flavour = generator(0, FLAVOUR_UPPER_BOUND);
+
+    printer.print(Printer::Student, id, Starting, flavour, bottles);
+
+    WATCard::FWATCard card = cardOffice.create(id, WATCARD_BALANCE);
+
+    VendingMachine *vendingMachine = nameServer.getMachine(id);
+    printer.print(Printer::Student, id, (char)Selecting, vendingMachine->getId());
+
+    while (bottles > 0) {
+        yield(generator(1, 10));
+
+        unsigned int cost = vendingMachine->cost();
+
+        WATCard *wcard = NULL;
+
+        do {
+            try {
+                wcard = card();
+            } catch (WATCardOffice::Lost &l) {
+                printer.print(Printer::Student, id, (char)Lost);
+                card = cardOffice.create(id, WATCARD_BALANCE);
+            }
+        } while (wcard == NULL);
+
+        try {
+            vendingMachine->buy((VendingMachine::Flavours)flavour, *card);
+        } catch (VendingMachine::Funds &f) {
+            unsigned int deposit = cost + WATCARD_BALANCE;
+
+            card = cardOffice.transfer(id, deposit, card);
+
+            wcard = NULL;
+
+            do {
+                try {
+                    wcard = card();
+                } catch (WATCardOffice::Lost &l) {
+                    printer.print(Printer::Student, id, (char)Lost);
+                    card = cardOffice.create(id, WATCARD_BALANCE);
+                }
+            } while (wcard == NULL);
+
+            continue;
+        } catch (VendingMachine::Stock &s) {
+            vendingMachine = nameServer.getMachine(id);
+            continue;
+        }
+
+        printer.print(Printer::Student, id, (char)Bought, card()->getBalance());
+
+        bottles -= 1;
+    }
+
+    printer.print(Printer::Student, id, (char)Finished);
+}
--- student.h
+++ student.h
@@ -6,10 +6,23 @@ _Task NameServer;
 _Task WATCardOffice;
 
 _Task Student {
-    void main();
-  public:
+public:
+    enum State {
+        Starting = 'S',
+        Selecting = 'V',
+        Bought = 'B',
+        Lost = 'L',
+        Finished = 'F'
+    };
     Student(Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
-            unsigned int maxPurchases);
+        unsigned int maxPurchases);
+private:
+    Printer &printer;
+    NameServer &nameServer;
+    WATCardOffice &cardOffice;
+    unsigned int id;
+    unsigned int maxPurchases;
+    void main();
 };
 
 #endif
