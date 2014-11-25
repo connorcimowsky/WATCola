4b1dc1173d035c04e72c9204f1bb74f5334081cf
Connor Cimowsky <connorcimowsky@me.com>
2014-11-24 22:50:21 -0500

    Add documentation for Bank.

--- bank.cc
+++ bank.cc
@@ -12,14 +12,16 @@ Bank::~Bank() {
 }
 
 void Bank::deposit(unsigned int id, unsigned int amount) {
-    balances[id] += amount;
-    withdrawals[id].signal();
+    balances[id] += amount;      // increment the appropriate account's balance by `amount`
+    withdrawals[id].signal();    // allow blocked withdrawers to re-check their balance
 }
 
 void Bank::withdraw(unsigned int id, unsigned int amount) {
+    // a busy wait is necessary here; `deposit` has no way of signalling a specific withdrawer,
+    // as we do not keep track of each task's desired withdrawal amount
     while (balances[id] < amount) {
         withdrawals[id].wait();
     }
 
-    balances[id] -= amount;
+    balances[id] -= amount;    // decrement the appropriate account's balance by `amount`
 }
--- bank.h
+++ bank.h
@@ -2,15 +2,17 @@
 #define BANK_H
 
 _Monitor Bank {
-
 public:
     Bank(unsigned int numStudents);
     ~Bank();
     void deposit(unsigned int id, unsigned int amount);
     void withdraw(unsigned int id, unsigned int amount);
 private:
-    unsigned int numStudents;
-    unsigned int *balances;
+    unsigned int numStudents;    // total number of students as defined in the configuration file
+    unsigned int *balances;      // an array of balances for each student's bank account
+
+    // an array of condition variables, one for each bank account; used for blocking tasks from
+    // withdrawing money until funds have been deposited into the respective bank account
     uCondition *withdrawals;
 };
 
