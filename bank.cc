c88cbe521d587cab68698c1fd80693be33a02967
Connor Cimowsky <connorcimowsky@me.com>
2014-11-24 21:55:02 -0500

    Switch back to using non-dynamic uConditions.

--- bank.cc
+++ bank.cc
@@ -3,29 +3,22 @@
 Bank::Bank(unsigned int numStudents) :
     numStudents(numStudents),
     balances(new unsigned int[numStudents]()),
-    withdrawals(new uCondition*[numStudents]) {
-    for (unsigned int i = 0; i < numStudents; i++) {
-        withdrawals[i] = new uCondition();
-    }
+    withdrawals(new uCondition[numStudents]) {
 }
 
 Bank::~Bank() {
-    for (unsigned int i = 0; i < numStudents; i++) {
-        delete withdrawals[i];
-    }
-
     delete[] balances;
     delete[] withdrawals;
 }
 
 void Bank::deposit(unsigned int id, unsigned int amount) {
     balances[id] += amount;
-    withdrawals[id]->signal();
+    withdrawals[id].signal();
 }
 
 void Bank::withdraw(unsigned int id, unsigned int amount) {
     while (balances[id] < amount) {
-        withdrawals[id]->wait();
+        withdrawals[id].wait();
     }
 
     balances[id] -= amount;
--- bank.h
+++ bank.h
@@ -11,7 +11,7 @@ public:
 private:
     unsigned int numStudents;
     unsigned int *balances;
-    uCondition **withdrawals;
+    uCondition *withdrawals;
 };
 
 #endif
