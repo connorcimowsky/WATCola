223e236da0765a63060554e7ab3fa67859161a52
Connor Cimowsky <connorcimowsky@me.com>
2014-11-25 15:06:53 -0500

    Clean up Truck and add documentation.

--- truck.cc
+++ truck.cc
@@ -3,9 +3,6 @@
 #include "nameServer.h"
 #include "bottlingPlant.h"
 #include "MPRNG.h"
-#include <iostream>
-
-using namespace std;
 
 Truck::Truck(Printer &prt, NameServer &nameServer, BottlingPlant &plant,
            unsigned int numVendingMachines, unsigned int maxStockPerFlavour) :
@@ -19,48 +16,65 @@ Truck::Truck(Printer &prt, NameServer &nameServer, BottlingPlant &plant,
 
 void Truck::main() {
     printer.print(Printer::Truck, (char)Start);
+
+    // get the list of registered vending machines from the name server; if any machines haven't been registered yet,
+    // NameServer will block `getMachineList` until the remaining machines have been registered
     VendingMachine** vendingMachines = nameServer.getMachineList();
 
     while(true) {
         yield(generator(1, 10));
         try {
+            // pick up the next shipment from the bottling plant
             plant.getShipment(cargo);
         } catch (BottlingPlant::Shutdown &s) {
+            // if the plant has shut down, break out of the loop and allow ourselves to terminate
             break;
         }
 
         printer.print(Printer::Truck, (char)PickedUp, bottlesLeft());
 
         for (unsigned int i = 0; i < numVendingMachines; i++) {
-            if(cargo[0] == 0 && cargo[1] == 0 && cargo[2] == 0 && cargo[3] == 0) {
+            if(cargo[VendingMachine::BluesBlackCherry] == 0
+            && cargo[VendingMachine::ClassicalCreamSoda] == 0
+            && cargo[VendingMachine::RockRootBeer] == 0
+            && cargo[VendingMachine::JazzLime] == 0) {
+                // if no bottles of soda have been produced for any flavor, no work is necessary for this production run
                 break;
             }
 
+            // get a pointer to the next vending machine needing to be restocked
             VendingMachine *machine = vendingMachines[machineForRestock];
 
             printer.print(Printer::Truck, (char)Begin, machine->getId(), bottlesLeft());
 
+            // get the number of stocked bottles for each flavour from the vending machine
             unsigned int *inventory = machine->inventory();
 
             unsigned int notReplenished = 0;
             for (unsigned int j = 0; j < VendingMachine::NumberOfFlavours; j++) {
+                // compute the number of bottles to stock for this flavour
                 unsigned int toStock = maxStockPerFlavour - inventory[j];
 
                 if(cargo[j] < toStock) {
+                    // if the provided shipment is not sufficient, increment `notReplenished` by the difference
                     notReplenished += toStock - cargo[j];
+
+                    // since we don't have enough bottles, stock as many as we can, i.e., `cargo[j]`
                     toStock = cargo[j];
                 }
 
+                // decrement the truck's cargo and increment the machine's inventory
                 cargo[j] -= toStock;
                 inventory[j] += toStock;
             }
             printer.print(Printer::Truck, (char)Unsuccessful, machine->getId(), notReplenished);
             printer.print(Printer::Truck, (char)Delivered, machine->getId(), bottlesLeft());
 
+            // let the vending machine know that the restocking has completed
             machine->restocked();
 
+            // select the next machine to restock in a round-robin fashion
             machineForRestock = (machineForRestock < numVendingMachines - 1) ? machineForRestock + 1 : 0;
-            // osacquire(cout) << endl << "Restock" << machineForRestock << endl;
         }
     }
 
@@ -68,6 +82,7 @@ void Truck::main() {
 }
 
 unsigned int Truck::bottlesLeft() {
+    // compute the total number of bottles (across all flavours) remaining on the truck
     unsigned int total = 0;
     for(unsigned int i = 0; i < VendingMachine::NumberOfFlavours; i++) {
         total += cargo[i];
--- truck.h
+++ truck.h
@@ -8,28 +8,37 @@ _Task NameServer;
 _Task BottlingPlant;
 
 _Task Truck {
+public:
+    Truck(Printer &prt, NameServer &nameServer, BottlingPlant &plant,
+           unsigned int numVendingMachines, unsigned int maxStockPerFlavour);
+private:
     enum States {
-        Start = 'S',
-        PickedUp = 'P',
-        Begin = 'd',
+        Start        = 'S',
+        PickedUp     = 'P',
+        Begin        = 'd',
         Unsuccessful = 'U',
-        Delivered = 'D',
-        Finish = 'F'
+        Delivered    = 'D',
+        Finish       = 'F'
     };
 
     Printer &printer;
     NameServer &nameServer;
     BottlingPlant &plant;
+
+    // constants as defined in ConfigParms
     unsigned int numVendingMachines;
     unsigned int maxStockPerFlavour;
+
+    // the number of bottles of each flavour being transported
     unsigned int cargo[VendingMachine::NumberOfFlavours];
+
+    // the current vending machine that needs to be restocked
     unsigned int machineForRestock;
 
-    void main();
+    // the total number of bottles (across all flavours) remaining on the truck
     unsigned int bottlesLeft();
-  public:
-    Truck(Printer &prt, NameServer &nameServer, BottlingPlant &plant,
-           unsigned int numVendingMachines, unsigned int maxStockPerFlavour);
+
+    void main();
 };
 
 #endif
